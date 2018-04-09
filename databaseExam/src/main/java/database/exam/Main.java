2aa6191354dd27e26a5684b0c0f2823decef5e86
mr.balalykin <miball4@gmail.com>
2018-04-09 17:06:02 +0300

    JDBC get connection and do simple request

--- databaseExam/src/main/java/database/exam/Main.java
+++ databaseExam/src/main/java/database/exam/Main.java
@@ -2,10 +2,7 @@ package database.exam;
 
 import com.mysql.fabric.jdbc.FabricMySQLDriver;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     private static final String URL = "jdbc:mysql://localhost:3306/mytestdb";
@@ -13,18 +10,29 @@ public class Main {
     private static final String PASSWORD = "root";
 
     public static void main(String[] args) {
-        Connection connection;
         try {
             Driver driver = new FabricMySQLDriver();
             DriverManager.registerDriver(driver);
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            if (!connection.isClosed()) {
-                System.out.println("Соединение установлено");
-            }
-            connection.close();
-            if (connection.isClosed()) {
-                System.out.println("Соединение закрыто");
-            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            Statement statement = connection.createStatement()) {
+
+            statement.execute("INSERT INTO animals(animal_name, animal_desc) VALUES ('name', 'desc');");//для вставки
+
+            int numChanges = statement.executeUpdate("UPDATE animals SET animal_name = 'Cat', animal_desc = 'Desc' WHERE id = 1;");//для апдейта
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM animals;");// для выборки
+
+            //region start
+            statement.addBatch("INSERT INTO animals(animal_name, animal_desc) VALUES ('Dog', 'desc');");
+            statement.addBatch("INSERT INTO animals(animal_name, animal_desc) VALUES ('Elefant', 'desc');");
+            statement.addBatch("INSERT INTO animals(animal_name, animal_desc) VALUES ('Lion', 'desc');");
+
+            statement.executeBatch(); // если надо выполнить несколько запросов за одну команду execute
+            statement.clearBatch(); // затирает старые запросы из  бэтчей
+            //endregion
         } catch (SQLException e) {
             e.printStackTrace();
         }
