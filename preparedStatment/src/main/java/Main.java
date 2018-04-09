d03ec6866a216c3a9f195486ea80f7b046c71509
mr.balalykin <miball4@gmail.com>
2018-04-09 18:57:40 +0300

    Prepared statment with TODO

---
+++ preparedStatment/src/main/java/Main.java
@@ -0,0 +1,61 @@
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.*;
+import java.util.Calendar;
+
+public class Main {
+    private static final String URL = "jdbc:mysql://localhost:3306/mytestdb";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public static void main(String[] args) {
+        Connection connection;
+        PreparedStatement preparedStatement = null;
+        final String INSERT_INTO = "INSERT INTO dish VALUES(?,?,?,?,?,?,?);";
+        final String GET_ALL = "SELECT * FROM dish;";
+        final String DELETE = "DELETE FROM dish where id = ?;";
+        try {
+            Driver driver = new FabricMySQLDriver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+            preparedStatement = connection.prepareStatement(DELETE);
+            preparedStatement.setInt(1,1);
+            preparedStatement.execute();
+            //preparedStatement = connection.prepareStatement(GET_ALL);
+            /*
+            preparedStatement = connection.prepareStatement(INSERT_INTO);
+            preparedStatement.setInt(1, 1);
+            preparedStatement.setString(2, "title");
+            preparedStatement.setString(3, "description");
+            preparedStatement.setFloat(4, 0.2f);
+            preparedStatement.setBoolean(5, true);
+            preparedStatement.setDate(6, new Date(Calendar.getInstance().getTimeInMillis()));
+            preparedStatement.setBlob(7, new FileInputStream("smile.png"));*/
+            //////////////////////////////////////
+            /*ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                int id = resultSet.getInt("id");
+                String title = resultSet.getString("title");
+                String desc = resultSet.getString("description");
+                float rating = resultSet.getFloat("rating");
+                boolean published = resultSet.getBoolean("published");
+                Date date = resultSet.getDate("created");
+                byte[] icon = resultSet.getBytes("icon");//TODO почему не работает с icon
+
+                System.out.println("id = " + id);
+                System.out.println("title = " + title);
+                System.out.println("desc = " + desc);
+                System.out.println("rating = " + rating);
+                System.out.println("published = " + published);
+                System.out.println("date = " + date);
+                System.out.println("icon = " + icon.length);
+
+            }*/
+        } catch (SQLException/* | FileNotFoundException*/ e) {
+            e.printStackTrace();
+        }
+    }
+}
