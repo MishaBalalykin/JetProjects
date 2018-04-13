d1e2ae83f6c6d6dbd95860f2e5c2fff77f6f9a30
mr.balalykin <miball4@gmail.com>
2018-04-13 11:29:51 +0300

    Not multythread transaction with save in map, test and Spring

--- moneyTransaction/src/test/java/TransactionTest.java
+++ moneyTransaction/src/test/java/TransactionTest.java
@@ -1,25 +1,34 @@
+import config.SpringConfig;
 import config.Transaction;
 import config.account.Account;
 import config.exceptions.NotEnoughMoneyException;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.math.BigDecimal;
 
 import static org.junit.Assert.*;
 
 public class TransactionTest {
-    /*ApplicationContext applicationContext = new ClassPathXmlApplicationContext("config.xml");
-    @Test(expected = NotEnoughMoneyException.class)
-    public void transferMoney() throws NotEnoughMoneyException {
-
-    }*/
-
-    private Transaction transaction = new Transaction();
-    private Account account = new Account(1, new BigDecimal(1200.15));
-    private Account toAccount = new Account(2, new BigDecimal(999.9));
+    @Autowired
+    static Transaction transaction;
+    @Autowired
+    static private Account account;
+    @Autowired
+    static private Account toAccount;
 
     @Test(expected = NotEnoughMoneyException.class)
     public void transferMoney() throws NotEnoughMoneyException {
+        AnnotationConfigApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext();
+
+        applicationContext.register(SpringConfig.class);
+        applicationContext.refresh();
+        transaction = applicationContext.getBean(Transaction.class);
+        account = applicationContext.getBean("first acc", Account.class);
+        toAccount = applicationContext.getBean("second acc", Account.class);
+
         assertTrue(transaction.transferMoney(account, toAccount, new BigDecimal(1200)));
         assertFalse(transaction.transferMoney(account, toAccount, new BigDecimal(-5)));
         assertFalse(transaction.transferMoney(account, toAccount, new BigDecimal(0)));
